894019f7c03100738b45f0932a925e0cab733f5f
Jin-Xu <adam@lostwire.net>
2020-09-12 16:19:03 +0200

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='estore.base',
+    version='0.0.1',
+    description='Event Store Base Classes',
+    author='Jnxy',
+    author_email='jnxy@lostwire.net',
+    license='BSD',
+    zip_safe=False,
+    include_package_data=True,
+    packages=setuptools.find_napespace_packages(include=['estore.*']))
