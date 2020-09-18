e524493e5f8ad404dcf13ffc61bb30c50ad086b3
Jin-Xu <adam@lostwire.net>
2020-09-18 13:51:06 +0100

    Adding MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='estore-base',
-    version='0.0.3',
+    version='0.0.4',
     description='Event Store Base Classes',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,6 +19,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Topic :: Database" ],
-    zip_safe=False,
-    include_package_data=True,
     packages=setuptools.find_namespace_packages(include=['estore.*']))
