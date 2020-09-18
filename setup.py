668c6d6a6a2ce32bf45eaa5d7ff733d35eb0fc68
Jin-Xu <adam@lostwire.net>
2020-09-18 09:04:56 +0100

    Adding README.md and some changes to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='estore.base',
     version='0.0.1',
     description='Event Store Base Classes',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jnxy',
     author_email='jnxy@lostwire.net',
-    classifiers = [
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Framework :: AsyncIO",
         "Environment :: Web Environment",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Topic :: Database" ]
-    license='MIT',
+        "Topic :: Database" ],
     zip_safe=False,
     include_package_data=True,
     packages=setuptools.find_namespace_packages(include=['estore.*']))
