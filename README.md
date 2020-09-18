319b23256d8fc07eff7a66e5d77c247757cceb97
Jin-Xu <adam@lostwire.net>
2020-09-18 13:28:20 +0100

    Updating README.md and setup.py

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 ## Installing
+From PyPi
+
 ```bash
-
 pip3 install estore-base
+```
 
+From GitHub
+
+```bash
+git clone git@github.com:lostwire/estore-base.git
+cd estore-base
+git install .
 ```
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='estore.base',
-    version='0.0.1',
+    name='estore-base',
+    version='0.0.3',
     description='Event Store Base Classes',
     long_description=long_description,
     long_description_content_type="text/markdown",
