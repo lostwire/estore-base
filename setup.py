4d25b7574b41c172d4972440b0ca6d1a53a0b29b
Jin-Xu <adam@lostwire.net>
2020-09-19 12:24:18 +0100

    Next version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='estore-base',
-    version='0.0.4',
+    version='0.0.5',
     description='Event Store Base Classes',
     long_description=long_description,
     long_description_content_type="text/markdown",
